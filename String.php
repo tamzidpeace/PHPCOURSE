614921076b9f90f84892a7b546c9433ccfc29179
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-12 16:08:43 +0600

    String and it's functions..

--- String.php
+++ String.php
@@ -3,22 +3,49 @@
 <html>
 
 <head>
-    <h1 >String</h1>
+    <h1>String</h1>
 </head>
 
-    <body>
+<body>
 
-        <?php
+<?php
 
-        $string = "hello world <br>";
-        $string2 = 'hello world2 <br>';
-        echo($string);
-        echo ($string2);
+$string = "hello world <br>";
+$string2 = 'hello world2 <br>';
+echo($string);
+echo($string2);
 
-        ?>
+$color = "red";
+$shirt = "tShirt";
+$combination = $color . " " . $shirt;
+echo($combination . "<br>");
+echo "$combination <br>";
 
-    </body>
+$fruit = 'banana <br>';
+echo $fruit;
+echo "{$shirt} is my favorite.<br>";
 
+
+?>
+
+
+
+<?php
+
+// string functions
+
+$things = "apple";
+$name = "Arafat";
+echo ($things. "<br>");
+echo(strtoupper($things. "<br>"));
+echo (strtolower($name. "<br>"));
+echo (ucfirst($things. "<br>"));
+
+//...
+
+?>
+
+</body>
 
 
 </html>
