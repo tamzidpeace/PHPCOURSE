79b29eb09ef4e2624e603150e0c6983bb64b1fd0
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-14 01:24:46 +0600

    Loops

---
+++ SwitchStatements.php
@@ -0,0 +1,31 @@
+<!DOCTYPE>
+
+<html>
+
+<head> <h1>Switch</h1> </head>
+
+<body>
+
+<?php
+
+echo ("! I love you.");
+
+$cse = "robin";
+
+switch ($cse) {
+
+    case "robin" :
+        echo ("<br> brilliant one");
+        break;
+    default :
+        echo "No one";
+        break;
+}
+
+
+
+
+?>
+
+
+</body>
