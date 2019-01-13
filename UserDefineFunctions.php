4416392295c86cae9cf3b08e596478cf7dc3b917
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-14 02:08:48 +0600

    function

---
+++ UserDefineFunctions.php
@@ -0,0 +1,31 @@
+<!DOCTYPE>
+
+<html>
+
+<head> <h1>function</h1> </head>
+
+<body>
+
+<?php
+
+echo ("! I love you.");
+
+function myFunction() {
+    echo " <br> hello world <br>";
+}
+
+
+
+function sum ($a, $b) {
+
+    return $a+ $b;
+}
+
+$result = sum(5, 6);
+echo $result;
+
+myFunction();
+
+?>
+
+</body>
