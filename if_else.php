a615fc8456467cd2c38518438b35182db78a2f8e
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-13 15:37:28 +0600

    if_else

---
+++ if_else.php
@@ -0,0 +1,20 @@
+<!DOCTYPE>
+
+<html>
+
+<head> <h1>if else</h1> </head>
+
+<body>
+
+<?php
+
+$a = 2;
+$b = 5;
+$c = 2;
+
+if($a>$b) echo ("false");
+elseif ($a<$b) echo ("true");
+else  echo ("true");
+
+?>
+</body>
