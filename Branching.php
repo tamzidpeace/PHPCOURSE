ac94987162abd02f227f3143a8045b1b09330998
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-14 01:43:07 +0600

    continue, break

--- Branching.php
+++ Branching.php
@@ -10,7 +10,20 @@
 
 echo ("! I love you. <br>");
 
+$name = array("sakib", "tamim", "mustafiz", "liton");
 
+
+for($i = 0; $i<3; $i++) {
+
+    if($name[$i] == "tamim")
+        continue;
+
+    elseif ($name[$i] == "sakib")
+        break;
+
+    else
+        echo ($name[$i] . "<br>");
+}
 
 ?>
 
