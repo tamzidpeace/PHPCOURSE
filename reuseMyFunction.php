2cc71fb437c39565c80cccd4341a4e1afeda930e
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-14 15:59:47 +0600

    reuseFunction

---
+++ reuseMyFunction.php
@@ -0,0 +1,24 @@
+<?php
+
+//include ("UserDefineFunctions.php");
+require ("UserDefineFunctions.php");
+?>
+
+<!DOCTYPE>
+
+<html>
+
+<head> <h1> Reuse function </h1> </head>
+
+<body>
+
+<?php
+
+//echo sum(2,3);
+
+
+?>
+
+</body>
+
+</html>
