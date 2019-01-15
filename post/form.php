3750bb64b0a520f8ccc047c818182bc72c3ced84
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-15 15:47:11 +0600

    post method

---
+++ post/form.php
@@ -0,0 +1,49 @@
+<?php
+
+if (isset($_POST["Submit"])) {
+
+    $Username = $_POST["Username"];
+
+    $Password = $_POST["Password"];
+
+    if ($Username == "arafat" && $Password == "arafat")
+        echo "welcome";
+
+    else echo "good bye";
+
+}
+
+?>
+
+
+<!DOCTYPE>
+
+<html>
+
+<head> <title>Form</title> </head>
+
+<body>
+
+<?php ?>
+
+<fieldset>
+    <legend> HTML 5 Form</legend>
+
+    <form action="" method="POST">
+
+        <label for = "Username">Username:</label>
+        <input id="Username" type="text" name="Username"> <br> <br>
+        <label for="Password">Password:</label>
+        &nbsp;
+        <input id="Password" type="Password" name="Password"> <br> <br>
+        <input type="Submit" name="Submit" value="Submitted">
+
+
+    </form>
+
+</fieldset>
+
+</body>
+
+
+</html>
