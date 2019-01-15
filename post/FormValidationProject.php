f0aee192faeecbefdfbd46c1f5825455f2f3ff8f
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-16 02:12:37 +0600

    form project design

---
+++ post/FormValidationProject.php
@@ -0,0 +1,37 @@
+<!DOCTYPE>
+
+<html>
+
+
+<head>
+
+    <title></title>
+
+</head>
+
+<body>
+
+<?php?>
+
+<form>
+	
+	Name:<br>
+	<input type="text" name="name"> * <br>
+	E-mail:<br>
+	<input type="email" name="email"> * <br>
+	Gender: *<br>
+	<input type="radio" name="male" value="Male"> Male
+	<input type="radio" name="female" value="Female"> Female <br>
+	website:<br>
+	<input type="text" name="website"> * <br>
+	Comment:<br>
+	<input type="text" name="comment"> <br>
+	<input type="submit" name="submit" value="submit">
+
+</form>
+
+
+
+</body>
+
+</html> 
