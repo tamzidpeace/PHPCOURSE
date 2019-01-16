c9e33b94913b36f14d204bc466d2aa1231bbdd54
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-17 00:39:28 +0600

    validate inputs of form

--- post/FormValidationProject.php
+++ post/FormValidationProject.php
@@ -5,34 +5,47 @@ $websiteError = "";
 $genderError = "";
 $emailError = "";
 
-if(isset($_POST['submit'])) {
+if (isset($_POST['submit'])) {
 
-    if(empty($_POST["name"])) {
+    if (empty($_POST["name"])) {
         $nameError = "name is required";
     } else {
         $name = Test_User_Input($_POST["name"]);
+        if(!preg_match("/^[a-zA-Z .]*$/", $name)) {
+            $nameError = "only letter and white space are allowed";
+        }
     }
 
-    if(empty($_POST["email"])) {
+    if (empty($_POST["email"])) {
         $emailError = "email is required";
     } else {
+
         $email = Test_User_Input($_POST["email"]);
+        if(!preg_match("/[A-zA-Z.-_]{3,}@[a-zA-Z.-_]{3,}.[a-zA-Z.-_]{2,}/", $email)) {
+            $emailError = "email address not valid";
+        }
+
     }
 
-    if(empty($_POST["website"])) {
+    if (empty($_POST["website"])) {
         $websiteError = "website is required";
     } else {
+
         $website = Test_User_Input($_POST["website"]);
+        if(!preg_match("/(https:)\/\/+[a-za-z0-9\-_%$\#\/?\~!&+*.]+\.[a-za-z0-9\-_%$\#\/?\~!&+*.]*/", $website)) {
+            $websiteError = "address is not valid";
+        }
     }
 
-    if(empty($_POST["gender"])) {
+    if (empty($_POST["gender"])) {
         $genderError = "gender is required";
     } else {
         $gender = Test_User_Input($_POST["gender"]);
     }
 }
 
-function Test_User_Input($data) {
+function Test_User_Input($data)
+{
     return $data;
 }
 
@@ -53,27 +66,26 @@ function Test_User_Input($data) {
 
 <body>
 
-<?php?>
+<? php ?>
 
 <form action="FormValidationProject.php" method="Post">
-	
-	Name:<br>
-	<input type="text" name="name"> * <?php echo $nameError; ?> <br>
-	E-mail:<br>
-	<input type="email" name="email"> <?php echo $emailError; ?> * <br>
-	Gender: <?php echo $genderError; ?> *<br>
-	<input type="radio" name="male" value="Male"> Male
-	<input type="radio" name="female" value="Female"> Female <br>
-	website:<br>
-	<input type="text" name="website"> * <?php echo $websiteError; ?> <br>
-	Comment:<br>
-<!--	<input type="text" name="comment"> <br>
--->
+
+    Name:<br>
+    <input type="text" name="name"> * <?php echo $nameError; ?> <br>
+    E-mail:<br>
+    <input type="email" name="email"> <?php echo $emailError; ?> * <br>
+    Gender: <?php echo $genderError; ?> *<br>
+    <input type="radio" name="male" value="Male"> Male
+    <input type="radio" name="female" value="Female"> Female <br>
+    website:<br>
+    <input type="text" name="website"> * <?php echo $websiteError; ?> <br>
+    Comment:<br>
+    <!--	<input type="text" name="comment"> <br>
+    -->
     <textarea name="Comment" rows="5" cols="25"></textarea> <br> <br>
     <input type="submit" name="submit" value="submit">
 
 </form>
-
 
 
 </body>
