7e70593d1aad294930874470efecc9b370887f53
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-18 12:14:22 +0600

    session

---
+++ Cookie/Session.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+?>
+<?php
+$_SESSION["name"] = "arafat";
+$name = $_SESSION["name"];
+echo $name;
+?>>
+<!DOCTYPE>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<?php
+?>
+</body>
+</html>
