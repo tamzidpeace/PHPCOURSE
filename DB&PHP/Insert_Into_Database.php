abb9bc4a2d281983583ff88da78fe372d28658cd
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-19 01:49:45 +0600

    insert data into database

--- DB&PHP/Insert_Into_Database.php
+++ DB&PHP/Insert_Into_Database.php
@@ -1,3 +1,29 @@
+<?php
+if (isset($_POST["Submit"])) {
+    if (!empty($_POST['Ename']) && !empty($_POST['Ssn'])) {
+        $Ename = $_POST['Ename'];
+        $Ssn = $_POST['Ssn'];
+        $Dept = $_POST['Dept'];
+        $Salary = $_POST['Salary'];
+        $Homeaddress = $_POST['Homeaddress'];
+
+        $Connection = mysql_connect('localhost', 'root', '');
+        $Selected = mysql_select_db('record', $Connection);
+
+        $Query = "INSERT INTO emp_record(ename, ssn, dept, salary, homeaddress)
+              values ('$Ename', '$Ssn', '$Dept', '$Salary', '$Homeaddress')";
+        $Execute = mysql_query($Query);
+
+        if ($Execute)
+            echo "information updated";
+        else
+            "something is wrong";
+    } else {
+        echo "At least fill name and ssn";
+    }
+}
+?>
+
 <!DOCTYPE>
 <html>
 <head>
@@ -7,11 +33,11 @@
 <div>
     <form action="Insert_Into_Database.php" , method="post">
         <fieldset>
-            Name:  <input type="text" name="Ename"> <br> <br>
-            Social Security No:  <input type="text" name="Ssn"> <br> <br>
-            Department:  <input type="text" name="Dept"> <br> <br>
-            Salary:  <input type="text" name="Salary"> <br> <br>
-            Home Address:  <textarea name="Homeaddress"> </textarea> <br><br>
+            Name: <input type="text" name="Ename"> <br> <br>
+            Social Security No: <input type="text" name="Ssn"> <br> <br>
+            Department: <input type="text" name="Dept"> <br> <br>
+            Salary: <input type="text" name="Salary"> <br> <br>
+            Home Address: <textarea name="Homeaddress"> </textarea> <br><br>
             <input type="submit" value="Insert Data" name="Submit">
         </fieldset>
     </form>
