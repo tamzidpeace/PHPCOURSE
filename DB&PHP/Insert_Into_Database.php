79792591a8b05d5d97eb41555094e396e5f65edd
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-19 01:07:25 +0600

    creating form for inserting information

---
+++ DB&PHP/Insert_Into_Database.php
@@ -0,0 +1,24 @@
+<!DOCTYPE>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<div>
+    <form action="Insert_Into_Database.php" , method="post">
+        <fieldset>
+            Name:  <input type="text" name="Ename"> <br> <br>
+            Social Security No:  <input type="text" name="Ssn"> <br> <br>
+            Department:  <input type="text" name="Dept"> <br> <br>
+            Salary:  <input type="text" name="Salary"> <br> <br>
+            Home Address:  <textarea name="Homeaddress"> </textarea> <br><br>
+            <input type="submit" value="Insert Data" name="Submit">
+        </fieldset>
+    </form>
+</div>
+
+<?php
+
+?>
+</body>
+</html>
