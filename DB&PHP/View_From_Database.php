90dfe987b6ab2592fa0cf18a8743e77aa50edb77
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-19 02:40:52 +0600

    view data from database

---
+++ DB&PHP/View_From_Database.php
@@ -0,0 +1,49 @@
+<!DOCTYPE>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<table width="1000" border="5" align="center">
+    <caption>View from database</caption>
+    <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>ssn</th>
+        <th>dept</th>
+        <th>salary</th>
+        <th>home</th>
+    </tr>
+
+    <?php
+
+    $Connection = mysql_connect('localhost', 'root', '');
+    $Selected = mysql_select_db('record', $Connection);
+    $ViewQuery = "Select * From emp_record";
+    $Execute = mysql_query($ViewQuery);
+    while ($DataRows = mysql_fetch_array($Execute)) {
+        $id = $DataRows['id'];
+        $Ename = $DataRows['ename'];
+        $Ssn = $DataRows['ssn'];
+        $Dept = $DataRows['dept'];
+        $Salary = $DataRows['salary'];
+        $Home = $DataRows['homeaddress'];
+
+        ?>
+        <tr>
+            <td> <?php echo $id; ?></td>
+            <td> <?php echo $Ename; ?></td>
+            <td> <?php echo $Ssn; ?></td>
+            <td> <?php echo $Dept; ?></td>
+            <td> <?php echo $Salary; ?></td>
+            <td> <?php echo $Home; ?></td>
+        </tr>
+    <?php } ?>
+
+</table>
+
+
+<?php
+?>
+</body>
+</html>
