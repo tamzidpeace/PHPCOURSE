f232a4fbdb450f2edb65bedbd60f87a845d1678b
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-20 02:21:28 +0600

    Start of CMS project

---
+++ PHPCMS/dashboard.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="english">
+<head>
+    <title></title>
+    <link rel="stylesheet" type="" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/adminstyles.css">
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-2">
+            <h1>Jazeb</h1>
+        </div>
+        <div class="col-sm-10">
+            <h1>About</h1>
+            <p>
+                XAMPP has been around for more than 10 years – there is a huge community behind it. You can get involved
+                by joining our Forums, adding yourself to the Mailing List, and liking us on Facebook, following our
+                exploits on Twitter, or adding us to your Google+ circles.
+            </p>
+        </div>
+
+    </div>
+</div>
+
+</body>
+</html>
